c571a79de5833590496cb4d4c7e7d64046947e29
BeaAmante <beatrizamante@hotmail.com>
2024-11-08 10:15:11 -0300

    Finalizando

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
+import logging
 from app.services.websocket_frame_process import app
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger("uvicorn")
 
-@app.get("/")
-async def get_index():
-    """Redirect to index.html in the static folder."""
-    return RedirectResponse(url="/static/index.html")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# @app.get("/")
+# async def get_index():
+#     """Redirect to index.html in the static folder. THIS IS FOR TESTING ONLY"""
+#     return RedirectResponse(url="/app/static/index.html")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    config = uvicorn.Config("main:app", 
+                            host="172.16.21.53", 
+                            port=443, 
+                            ssl_certfile="./cert",
+                            ssl_keyfile="./key"
+                            )
+    server = uvicorn.Server(config)
+    server.run()
     
 
-    # auth_service = FaceController()
-
-    # try:
-    #      auth_service.authenticate_user_clientside()
-    # except KeyboardInterrupt:
-    #     print("Stopping the facial recognition API...")
-    # except Exception as e:
-    #     print("An error occured: ", e)
 
